a399afa2d649bd1bc711d638c91d82f69a91206f
masdefry <muhammaddefryan@gmail.com>
2021-02-25 11:18:13 +0700

    Map Data

--- src/Data1.jsx
+++ src/Data1.jsx
@@ -1,4 +1,3 @@
-import { render } from '@testing-library/react'
 import React from 'react'
 import Axios from 'axios'
 
@@ -6,23 +5,48 @@ const linkAPI = 'https://randomuser.me/api/'
 
 export default class Data1 extends React.Component{
 
+    state = {
+        data: null
+    }
+
     getData = () => {
-        // Axios.get(linkAPI)
-        // .then((response) => {
-        //     console.log(response)
-        // })
-        // .catch((error) => {
-        //     console.log(error)
-        // })
+        Axios.get(linkAPI)
+        .then((response) => {
+            this.setState({data: response.data.results})
+        })
+        .catch((error) => {
+            console.log(error)
+        })
     }
 
     render(){
+        if(this.state.data === null){
+            return(
+                <div>
+                    <h1>
+                        Fetch Data API - 1
+                    </h1>
+                    <input type='button' value='Ambil Data' onClick={this.getData} />
+                </div>
+            )
+        }
+
         return(
             <div>
                 <h1>
-                    Fetch Data API - 1
+                    Data :
                 </h1>
-                <input type='button' value='Ambil Data' onClick={this.getData} />
+                {
+                    this.state.data.map((value, index) => {
+                        return(
+                            <div key={index}>
+                                <h1>
+                                    {value.email}
+                                </h1>
+                            </div>
+                        )
+                    })
+                }
             </div>
         )
     }
---
+++ src/Notes.js
@@ -0,0 +1,10 @@
+// Install Bootstrap :
+//      - Menggunakan CDN : 1. Copy link : <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+//                          2. Paste : public/index.html
+
+// Synchronous & Asynchronous :
+//      - Sync : Dia menjalankan baris per-baris secara berurutan
+//        Ex. console.log('...')
+//      - Async : Dia menjalankan baris per-baris secara berurutan TAPI
+//                Dia nggak nunggu baris sebelumnya selesai
+//        Ex. Axios ---> Dia menghasilkan promise yg belum diketahui responsenya apa
