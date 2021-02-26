fd3f2aadf53c9bc08af07f9c9ea00e07e7b5b4fd
masdefry <muhammaddefryan@gmail.com>
2021-02-26 11:18:05 +0700

    Submit Data

--- src/FetchFakeAPI.jsx
+++ src/FetchFakeAPI.jsx
@@ -25,6 +25,35 @@ export default class FetchFakeAPI extends React.Component{
         })
     }
 
+    onSubmitData = () => {
+        // 1. Ambil value inputan user
+        let inputUsername = this.refs.username.value
+        let inputEmail = this.refs.email.value
+        let inputPassword = this.refs.password.value
+
+        // 2. Validasi inputan user (optional)
+        if(inputUsername && inputEmail && inputPassword){
+            // 3. Kirim data user menuju API
+            Axios.post(linkAPI, {username: inputUsername, email: inputEmail, password: inputPassword})
+            .then((res) => {
+                if(res.status === 201){
+                    alert('Data Berhasil Ditambah')
+                    this.onGetData()
+
+                    // Supaya Input Form Menjadi Koson Kembali 
+                    this.refs.username.value = ''
+                    this.refs.email.value = ''
+                    this.refs.password.value = ''
+                }
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+        }else{
+            alert('Masukan Seluruh Data!')
+        }
+    }
+
     mapDataUsers = () => {
         return this.state.dataUsers.map((value, index) => {
             return(
@@ -67,7 +96,7 @@ export default class FetchFakeAPI extends React.Component{
                     }
                     </tbody>
                 </table>
-
+                    
                 {
                     this.state.showForm?
                         <div className='mt-5'>
@@ -75,16 +104,16 @@ export default class FetchFakeAPI extends React.Component{
                                 Inpurt Form
                             </h1>
                             <div>
-                                <input type='text' placeholder='Input Username' className='form-control w-50' />
+                                <input type='text' ref='username' placeholder='Input Username' className='form-control w-50' />
                             </div>
                             <div className='mt-3'>
-                                <input type='text' placeholder='Input Email' className='form-control w-50' />
+                                <input type='text' ref='email' placeholder='Input Email' className='form-control w-50' />
                             </div>
                             <div className='mt-3'>
-                                <input type='text' placeholder='Input Password' className='form-control w-50' />
+                                <input type='text' ref='password' placeholder='Input Password' className='form-control w-50' />
                             </div>
 
-                            <input type='button' value='Submit Data' className='btn btn-primary mt-3' />
+                            <input type='button' value='Submit Data' className='btn btn-primary mt-3' onClick={this.onSubmitData} />
                             <input type='button' value='Cancel' className='btn btn-danger mt-3' onClick={() => this.setState({showForm: false})} />
                         </div>
                     :
